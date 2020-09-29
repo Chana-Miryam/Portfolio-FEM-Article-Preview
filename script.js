ba92102e31abdb21ea98fdb98f21e27d24065ee6
Channah <code.challenge.cv@gmail.com>
2020-09-29 12:22:24 +0100

    Basic mobile version Completed

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function shareClicked() {
+    document.getElementById("avatar-container").style.display = "none";
+    document.getElementById("avatar-container-active").style.display = "grid"; // On click elem vanishes 
+};
+
+function shareClicked2() {
+    document.getElementById("avatar-container").style.display = "grid";
+    document.getElementById("avatar-container-active").style.display = "none"; // On click elem vanishes 
+};
